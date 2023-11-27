9aa5d052e7db70f534c947b43ed527e8e7105a0c
milan1248 <miba1248@gmail.com>
2023-11-27 12:51:16 +0100

    serve file

--- myApp.js
+++ myApp.js
@@ -3,9 +3,14 @@ let app = express();
 
 console.log("Hello World");
 
+// app.get("/", (req,res)=>{
+//     res.send('Hello Express')
+// }
+// )
+
 app.get("/", (req,res)=>{
-    res.send('Hello Express')
-}
+        res.sendFile(__dirname + '/views/index.html')
+    }
 )
 
 
