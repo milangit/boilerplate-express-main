1b457e5f1cbe128219df2c59e7de6a263ce1ce69
milan1248 <miba1248@gmail.com>
2023-11-27 21:34:18 +0100

    updt

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 app.use('/public', express.static(__dirname + '/public'))
 app.use((req, res, next)=>{
-    console.log(`${req.method}`);
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
   }
 )
