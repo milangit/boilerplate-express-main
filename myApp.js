3435720e4a90dd65da3f6b8e25757d968094c3ca
milan1248 <miba1248@gmail.com>
2023-11-28 11:52:35 +0100

    fix

--- myApp.js
+++ myApp.js
@@ -39,7 +39,8 @@ app.get("/json", (req,res)=>{
 
 
 app.get("/:word/echo", (req,res)=>{
-    res.send(`{echo: ${req.params.word}}`)
+    // res.send(`{echo: ${req.params.word}}`)
+    res.send(`{"echo": "${req.params.word}"}`);
 }
 )
 
